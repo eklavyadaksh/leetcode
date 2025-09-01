89252f6a65dbb803ec605f82267cea5e4c65b822
eklavyadaksh <eklavyadaksh4@gmail.com>
2025-09-01 18:54:26 +0530

    Create 3324-SplitTheArray

---
+++ 3324-SplitTheArray/3324-SplitTheArray.cpp
@@ -0,0 +1,14 @@
+// Last updated: 01/09/2025, 18:54:26
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) { 
+        int n = nums.size(); 
+        int count[101];
+        for(int i = 0; i < n;i++){
+            if(++count[nums[i]]>=3){
+                return false;
+            }
+        }
+        return true;
+    }
+};
