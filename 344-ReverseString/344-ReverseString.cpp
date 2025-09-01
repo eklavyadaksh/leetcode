b0490e0d6472c0cfdd93de2f6da6222eeb9ef28a
eklavyadaksh <eklavyadaksh4@gmail.com>
2025-09-01 22:21:52 +0530

    File updated at 01/09/2025, 22:21:52

--- 344-ReverseString/344-ReverseString.cpp
+++ 344-ReverseString/344-ReverseString.cpp
@@ -1,15 +1,23 @@
-// Last updated: 01/09/2025, 22:17:52
+// Last updated: 01/09/2025, 22:21:52
 class Solution {
 public:
     void reverseString(vector<char>& s) {
         int n=s.size();
-        char r[100];
+        // char r[100];
         char temp;
-        for(int i=0;i<n/2;i++){
-            std::cout<<s[i]<<std::endl;
+        // for(int i=0;i<n/2;i++){
+        //     std::cout<<s[i]<<std::endl;
+            
+        // }
+        int i = 0;
+        int j = n - 1;
+        while(i < j){
             temp = s[i];
             s[i] = s[n-i-1];
             s[n-i-1] = temp;
+            i++;
+            j--;
         }
+
     }
 };
